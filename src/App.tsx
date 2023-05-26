be158e7bbea8b15d74259df4ba846ce002606108
Jefferson de Guzman <Jefferson30.jdg@gmail.com>
2023-05-26 18:58:39 +0800

    directory restructure

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./styles/App.css";
 import { Home } from "./components/views/Home";
 import React from "react";
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.css";
+import "./styles/index.css";
 import App from "./App.tsx";
 import React from "react";
 import ReactDOM from "react-dom/client";
