55a2304418bf8f4352bf74d7d71295304c9feb79
Robert Daipan <daipanrv@gmail.com>
2023-05-29 19:49:03 +0800

    material and sizing outline layout

--- src/components/AccordionInput/AccordionInput.tsx
+++ src/components/AccordionInput/AccordionInput.tsx
@@ -85,13 +85,29 @@ export const AccordionInput: React.FC = () => {
         <Accordion.Item eventKey='1'>
           <Accordion.Header><img src='src/assets/icons/material-sizing.png' alt='icon' /> Material & Sizing</Accordion.Header>
           <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            <div className="material-and-sizing-container">
+            <p>Frame</p>
+              <div className="frame-type">
+                <div className="col-4 frame-selection">
+                </div>
+                <div className="col-4 frame-selection">
+                </div>
+                <div className="col-4 frame-selection">
+                </div>
+              </div>
+              <p>Size</p>
+              <div className="sizing-container">
+                
+                <div className="col-6 frame-selection">
+                </div>
+                <div className="col-6 frame-selection">
+                </div>
+                <div className="col-6 frame-selection">
+                </div>
+                <div className="col-6 frame-selection">
+                </div>
+              </div>
+            </div>
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey='2'>
