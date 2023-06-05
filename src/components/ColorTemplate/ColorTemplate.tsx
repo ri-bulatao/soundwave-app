61d9b81fb20d81401f96914085b6f958d2632ff9
Robert Daipan <daipanrv@gmail.com>
2023-06-05 12:18:31 +0800

    update of defualt values and color selection update

--- src/components/ColorTemplate/ColorTemplate.tsx
+++ src/components/ColorTemplate/ColorTemplate.tsx
@@ -2,26 +2,26 @@ import React, { useState } from 'react'
 import './ColorTemplate.css'
 
 interface ToggleButtonProps {
-  options: Array<{ key: string, image: string }>
+  options: Array<{ key: string, image: string, view: string }>
   handleColorSelection: (value: string) => void
 }
 
 const ColorTemplate: React.FC<ToggleButtonProps> = ({ options, handleColorSelection }) => {
   const [selectedOption, setSelectedOption] = useState('option_0')
-  const handleOptionChange = (value: string): any => {
-    setSelectedOption(value)
-    handleColorSelection(value)
+  const handleOptionChange = (value: any): any => {
+    setSelectedOption(value.key)
+    handleColorSelection(value.view+" "+value.key)
   }
   return (
     <>
         <div className='template-color'>
             {options.map((option: any) => (
-                <label className={`col-3 frame-color-selection frame-color ${selectedOption === option.key && 'active'}`} key={option.key}>
+                <label className={`col-3 frame-color-selection frame-color ${ option.view } ${selectedOption === option.key && 'active'}`} key={option.key}>
                 <input
                     type="radio"
                     value={option.key}
                     checked={selectedOption === option.key}
-                    onChange={() => handleOptionChange(option.key)}
+                    onChange={() => handleOptionChange(option)}
                 />
                 <img src={option.image} alt={option.key} />
                 </label>
