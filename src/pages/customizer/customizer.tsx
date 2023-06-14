b63fa66d338ace2e43db0b1eb2c2a0c59024fa3e
Russell Ian Bulatao <108667456+ri-bulatao@users.noreply.github.com>
2023-06-14 18:39:35 +0800

    Added Accordion Fixes

--- src/pages/customizer/customizer.tsx
+++ src/pages/customizer/customizer.tsx
@@ -10,6 +10,7 @@ import ColorTemplate from '../../components/ColorTemplate/ColorTemplate'
 import ConfirmationModal from '../../components/ConfirmationModal/ConfirmationModal'
 import '~/pages/customizer/customizer.scss'
 
+
 export const Customizer: React.FC = () => {
   const [audioFile, setAudioFile] = useState<File | null>(null)
   const [audioBuffer, setAudioBuffer] = useState<AudioBuffer | null>(null)
@@ -152,6 +153,7 @@ export const Customizer: React.FC = () => {
                 </Accordion.Item>
               </Accordion>
               : <Accordion defaultActiveKey={['0']} className={'main-accordion-layout'}>
+                {/* Upload Accordion */}
                 <Accordion.Item eventKey='0'>
                   <Accordion.Header className={`upload-header ${audioBuffer !== null ? 'file-uploaded' : ''}`}>
                     <div className='upload-header'>
@@ -182,8 +184,19 @@ export const Customizer: React.FC = () => {
                     }
                   </Accordion.Body>
                 </Accordion.Item>
+                {/* Material Accordion */}
                 <Accordion.Item eventKey='1'>
-                  <Accordion.Header className={`material-and-sizing-header ${(selectedFrame !== '' && selectedSizing !== '' && audioBuffer !== null) ? 'material-sizing-selected' : ''}`} ><div className='upload-header'><div><img src='src/assets/icons/svg/material.svg' alt='icon' className='accordion-icon' /> Material & Sizing</div><p className='upload-desc'>Upload yuor media to continue:</p></div></Accordion.Header>
+                  <Accordion.Header className={`material-and-sizing-header ${(selectedFrame !== '' && selectedSizing !== '' && audioBuffer !== null) ? 'material-sizing-selected' : ''}`} >
+                    <div className='upload-header'>
+                      <div>
+                        <svg className='accordion-icon' width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M9 5H13.8C14.9201 5 15.4802 5 15.908 5.21799C16.2843 5.40973 16.5903 5.71569 16.782 6.09202C17 6.51984 17 7.07989 17 8.2V13M1 5H5M17 17V21M21 17L8.2 17C7.07989 17 6.51984 17 6.09202 16.782C5.71569 16.5903 5.40973 16.2843 5.21799 15.908C5 15.4802 5 14.9201 5 13.8V1" stroke="#767676" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                        Material & Sizing
+                      </div>
+                      <p className='upload-desc'>Upload yuor media to continue:</p>
+                    </div>
+                  </Accordion.Header>
                   <Accordion.Body>
                     <div className="material-and-sizing-container">
                       <p>Frame Type</p>
@@ -193,8 +206,19 @@ export const Customizer: React.FC = () => {
                     </div>
                   </Accordion.Body>
                 </Accordion.Item>
+                {/* Order Review Accordion */}
                 <Accordion.Item eventKey='2'>
-                  <Accordion.Header><div className='upload-header'><div><img src='src/assets/icons/svg/order.svg' alt='icon' className='accordion-icon'/> Order Review</div><p className='upload-desc'>Here is your order details:</p></div></Accordion.Header>
+                  <Accordion.Header>
+                    <div className='upload-header'>
+                      <div>
+                        <svg className='accordion-icon' width="18" height="22" viewBox="0 0 18 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M11 10H5M7 14H5M13 6H5M17 5.8V16.2C17 17.8802 17 18.7202 16.673 19.362C16.3854 19.9265 15.9265 20.3854 15.362 20.673C14.7202 21 13.8802 21 12.2 21H5.8C4.11984 21 3.27976 21 2.63803 20.673C2.07354 20.3854 1.6146 19.9265 1.32698 19.362C1 18.7202 1 17.8802 1 16.2V5.8C1 4.11984 1 3.27976 1.32698 2.63803C1.6146 2.07354 2.07354 1.6146 2.63803 1.32698C3.27976 1 4.11984 1 5.8 1H12.2C13.8802 1 14.7202 1 15.362 1.32698C15.9265 1.6146 16.3854 2.07354 16.673 2.63803C17 3.27976 17 4.11984 17 5.8Z" stroke="#767676" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                        Order Review
+                      </div>
+                      <p className='upload-desc'>Here is your order details:</p>
+                    </div>
+                  </Accordion.Header>
                   <Accordion.Body>
                     <ul className="order-preview-container">
                       <li className="order-item">
