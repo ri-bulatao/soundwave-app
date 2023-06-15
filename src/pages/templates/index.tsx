4861272aa59be2641de6b2151caacdb372f97894
devmode2022 <devmode2022@gmail.com>
2023-06-15 11:41:02 +0800

    Added templtes page

--- src/pages/templates/index.tsx
+++ src/pages/templates/index.tsx
@@ -1,9 +1,11 @@
 import '../../styles/main.scss'
+import './templates.css'
 import React, { useEffect } from 'react'
 import type { RootState } from '../../redux/store'
 import { useDispatch, useSelector } from 'react-redux'
 import { load } from '../../redux/reducers/templates'
 import { templatesData } from '../../config/initialTemplates'
+import TemplateCard from '../../components/TemplateCard'
 
 const Templates: React.FC = () => {
     
@@ -12,20 +14,22 @@ const Templates: React.FC = () => {
     
     useEffect(() => {
         dispatch(load(templatesData))
-        console.log('Current State', templates)
-        debugger
     }, [])
 
     return (
-        <div>
-            {
-                templates.map(template => (
-                    <div>
-                        <h1>{template.title.text}</h1>
-                        <p>{template.subTitle.text}</p>
-                    </div>
-                ))
-            }
+        <div className="container">
+            <div className="title-container">
+                <h1 className="title">Templates (20)</h1>
+                <button className="filter">Filters <img src="/src/assets/icons/filter.png" alt="" className="icon" /></button>
+            </div>
+            <div className="cards-container">
+                {
+                    templates.map(template => <TemplateCard template={template} />)
+                }
+            </div>
+            <div className="action-container">  
+                <button className="load-more">Load more</button>
+            </div>
         </div>
     )
 }
